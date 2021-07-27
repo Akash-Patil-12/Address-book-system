dc05456b0cb50c50aa3b873baecaddae8ba6fdd5
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-27 11:51:08 +0530

    [Akash] Add : Create a Contacts in Address book

---
+++ Address Book System/Contacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book_System
+{
+    class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public long PhoneNumber { get; set; }
+        public String Email { get; set; }
+    }
+}
--- Address Book System/Program.cs
+++ Address Book System/Program.cs
@@ -6,7 +6,40 @@ namespace Address_Book_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address book program");
-        }
+            string firstName,lastName,address,city,state,email;
+            int zip;
+            long phoneNumber;
+            GetUserData();
+            Contacts contacts = new Contacts()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address=address,
+                City=city,
+                State=state,
+                Zip=zip,
+                PhoneNumber=phoneNumber,
+                Email=email
+            };  
+            void GetUserData()
+            {
+                Console.WriteLine("Enter First Name");
+                firstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                lastName = Console.ReadLine();
+                Console.WriteLine("Enter address");
+                address = Console.ReadLine();
+                Console.WriteLine("Enter city");
+                city = Console.ReadLine();
+                Console.WriteLine("Enter state");
+                state = Console.ReadLine();
+                Console.WriteLine("Enter zip code");
+                zip = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter phone number");
+                phoneNumber = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter email");
+                email = Console.ReadLine();
+            }
+        }       
     }
 }
